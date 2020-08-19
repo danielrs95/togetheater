f3b901bcfa1b262f1be3935562c8f718d7c70d05
Daniel <danielrs9504@gmail.com>
2020-08-18 21:01:59 -0500

    Refactor code, made master component for views

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { BrowserRouter, Switch , Route} from "react-router-dom";
 import './App.css';
 import NavbarComponent from './components/Navbar.component.js';
 import Dashboard from './components/Dashboard.js';
-import Trending from './components/Trending.component.js';
-import NowPlaying from './components/NowPlaying.component.js';
-import Popular from './components/Popular.component.js';
-import TopRated from './components/TopRated.component.js';
-import Upcoming from './components/Upcoming.component.js';
+import {NowPlaying, Trending, Popular, TopRated, Upcoming} from './views/Views.component.js';
 import Login from './components/Login.component.js';
 import Register from './components/Register.component.js';
 import Wishlist from './components/Wishlist.component.js';
@@ -16,25 +12,25 @@ import Auth from './context/store/Auth'
 
 function App() {
   return (
-  	<Auth>
-	  	<BrowserRouter>
-		      <NavbarComponent />
-		      <br/>
-		      <Switch>
-		      	<Route path="/" exact component={Dashboard} />
-		      	<Route path="/Trending" exact component={Trending} />
-		      	<Route path="/NowPlaying" exact component={NowPlaying} />
-		      	<Route path="/Popular" exact component={Popular} />
-		      	<Route path="/TopRated" exact component={TopRated} />
-		      	<Route path="/Upcoming" exact component={Upcoming} />
-		      	<Route path="/Wishlist" exact component={Wishlist} />
-		      	<Route path="/Login"  component={Login} />
-		      	<Route path="/Register"  component={Register} />
-		      </Switch>
-          <Footer/>
-	    </BrowserRouter>
+    <Auth>
+      <BrowserRouter>
+        <NavbarComponent />
+        <br/>
+        <Switch>
+          <Route path="/" exact component={Dashboard} />
+          <Route path="/Trending" exact component={Trending} />
+          <Route path="/NowPlaying" exact component={NowPlaying} />
+          <Route path="/Popular" exact component={Popular} />
+          <Route path="/TopRated" exact component={TopRated} />
+          <Route path="/Upcoming" exact component={Upcoming} />
+          <Route path="/Wishlist" exact component={Wishlist} />
+          <Route path="/Login"  component={Login} />
+          <Route path="/Register"  component={Register} />
+        </Switch>
+        <Footer/>
+      </BrowserRouter>
     </Auth>
-  );
+    );
 }
 
 export default App;
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -7,37 +7,6 @@ import AuthGlobal from "../context/store/AuthGlobal";
 export default function Dashboard(){
 	const context = useContext(AuthGlobal);
 
-	// const [trending, setTrending] = useState([])
-	// const [nowPlaying, setNowPlaying] = useState([])
-	// const [popular, setPopular] = useState([])
-	// const [topRated, setTopRated] = useState([])
-	// const [upcoming, setUpcoming] = useState([])
-
-	// useEffect(()=>{
-	// 	const fetchData = () => {
-	// 		axios.all([
-	// 			axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=${process.env.REACT_APP_API_KEY}`),
-	// 			axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}`),
-	// 			axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}`),
-	// 			axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}`),
-	// 			axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_API_KEY}`)
-	// 		])
-	// 		.then(axios.spread((trendingData, nowPlayingData, popularData, topRatedData, upcomingData)=>{
-	// 			const trending = trendingData.data.results.map(movie=>{return movie})
-	// 			const nowPlaying = nowPlayingData.data.results.map(movie=>{return movie})
-	// 			const popular = popularData.data.results.map(movie=>{return movie})
-	// 			const topRated = topRatedData.data.results.map(movie=>{return movie})
-	// 			const upcoming = upcomingData.data.results.map(movie=>{return movie})
-	// 			setTrending(trending)
-	// 			setNowPlaying(nowPlaying)
-	// 			setPopular(popular)
-	// 			setTopRated(topRated)
-	// 			setUpcoming(upcoming)
-	// 		}))
-	// 	}
-	// 	fetchData();
-	// }, [])
-
 	return(
 		<div className="mt-5">
 			<Link className="dashboardTitle" to="/trending">Trending</Link>
